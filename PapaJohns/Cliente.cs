7d1c89027f2596eab619792fc67ad7a950018258
Josi Urutti <josi_taringa@hotmail.com>
2019-06-23 17:09:16 -0300

    You know im something of a programmer myself

---
+++ PapaJohns/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaJohns
+{
+    class Cliente
+    {
+
+        public int id { get; set; }
+        private int consumos;
+        public int Consumos { get { return consumos; } set { this.consumos += value;  } }
+
+        public Cliente()
+        {
+
+        }
+    }
+}
---
+++ PapaJohns/Mesa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaJohns
+{
+    class Mesa
+    {
+        public int id { get { return id; } set { this.id = value;  } }
+
+        private int tamaño;
+        public int Tamaño { get { return tamaño; } set { this.tamaño = value;  } }
+
+        private string forma; //redonda, cuadrada o rectangular
+        public string Forma { get { return forma; } set { this.forma = value; } }
+
+        private List<bool> silla; //cantidad y disponibilidad(depende de la cantidad de nodos q tenga es la cantidad y la disponibilidad de si es true o false.
+        private string estado; //libre, ocupada o reservada.
+        public string Estado { get { return estado; } set { this.estado = value; } }
+
+        public Mozo mozito;
+
+        public Mesa()
+        {
+
+        }
+
+
+
+    }
+}
