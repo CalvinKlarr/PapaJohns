151026bb07872d6e679bd3f5add9b0b5cc1665f1
Juan Andres Calvo <juan_andres_calvo@yahoo.com.ar>
2019-06-24 18:49:16 -0300

    working dragdrop

--- PapaJohns/MainWindow.xaml.cs
+++ PapaJohns/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace PapaJohns
         {
             InitializeComponent();
         }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
     }
 }
--- PapaJohns/Window1.xaml.cs
+++ PapaJohns/Window1.xaml.cs
@@ -19,18 +19,85 @@ namespace PapaJohns
     /// </summary>
     public partial class Window1 : Window
     {
+        private Point mouseClick;
+        private Image draggedImage;
         public Window1()
         {
             InitializeComponent();
             toolBox.MouseDoubleClick += ToolBox_MouseDoubleClick;
+
+
         }
 
         private void ToolBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if(toolBox.SelectedItem != null)
+            if (toolBox.SelectedItem != null)
             {
-                MessageBox.Show(toolBox.SelectedItem.ToString());
+                ListBoxItem lbItem = (ListBoxItem)toolBox.SelectedItem;
+                
+
+                StackPanel stack = (StackPanel)lbItem.Content;
+
+                TextBlock text;
+                string selected = "";
+                foreach (var child in stack.Children)
+                {
+                    if(child.GetType().ToString() == "System.Windows.Controls.TextBlock")
+                    {
+                        text = (TextBlock) child;
+                        selected = text.Text;
+                    }
+                }
+                
+                if(selected == " Mesa")
+                {
+                    Image table = new Image();
+                    table.Source = new BitmapImage(new Uri("table.png", UriKind.Relative));
+                    table.Height = 50;
+                    table.Width = 50;
+                    Canvas.SetLeft(table, 0);
+                    Canvas.SetTop(table, 0);
+                    designSpace.Children.Add(table);
+                    
+                }
             }
+        }
+
+        private void DesignSpace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var image = e.Source as Image;
+
+            if(image != null && designSpace.CaptureMouse())
+            {
+                mouseClick = e.GetPosition(designSpace);
+                draggedImage = image;
+                Panel.SetZIndex(draggedImage, 1);
+            }
+
+        }
+
+        private void DesignSpace_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(draggedImage != null)
+            {
+                designSpace.ReleaseMouseCapture();
+                Panel.SetZIndex(draggedImage, 0);
+                draggedImage = null;
+            }
+
+        }
+
+        private void DesignSpace_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(draggedImage != null)
+            {
+                var position = e.GetPosition(designSpace);
+                var offset = position - mouseClick;
+                mouseClick = position;
+                Canvas.SetLeft(draggedImage, Canvas.GetLeft(draggedImage) + offset.X);
+                Canvas.SetTop(draggedImage, Canvas.GetTop(draggedImage) + offset.Y);
+            }
+
         }
     }
 }
