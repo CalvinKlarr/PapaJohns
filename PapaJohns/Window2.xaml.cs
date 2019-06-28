e28046621a113dcefda4e7e1ea6c86fb917c271d
Juan Andres Calvo <juan_andres_calvo@yahoo.com.ar>
2019-06-28 06:31:15 -0300

    saving

--- PapaJohns/Window2.xaml.cs
+++ PapaJohns/Window2.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using PapaJohnsCODE;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace PapaJohns
 {
@@ -22,14 +25,61 @@ namespace PapaJohns
     public partial class Window2 : MetroWindow
     {
         List<Mesa> mesas;
-        public Window2(Canvas design, List<Mesa> dict)
+        public Window2()
         {
-            designSpace = design;
-            mesas = dict;
             InitializeComponent();
+            //OPEN CANVAS
 
-           
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //  dlg.FileName = "CANVAS";
+            dlg.DefaultExt = ".xaml"; // Default file extension
+            dlg.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                Canvas canvus = Window1.DeSerializeXAML(filename) as Canvas;
+
+                // Add all child elements (lines, rectangles etc) to canvas
+                while (canvus.Children.Count > 0)
+                {
+                    UIElement obj = canvus.Children[0]; // Get next child
+                    canvus.Children.Remove(obj); // Have to disconnect it from result before we can add it
+                    designCanvas.Children.Add(obj); // Add to canvas
+                }
+                designCanvas.Background = canvus.Background;
+            }
+
+            //OPEN RELACIONES
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Mesa>));
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            // ofd.FileName = "RELACIONES";
+            ofd.DefaultExt = ".xaml"; // Default file extension
+            ofd.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            ofd.ShowDialog();
+
+            using (Stream reader = new FileStream(ofd.FileName, FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                mesas = (List<Mesa>)serializer.Deserialize(reader);
+            }
+
+            
+
+            
+            
+
         }
+        
+     
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
