5e9cb92c29ae5983bd796c2f925e68c565cf4ab9
Josi Urutti <josi_taringa@hotmail.com>
2019-06-28 06:33:03 -0300

    JOSIKPO

--- PapaJohns/Window2.xaml.cs
+++ PapaJohns/Window2.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using PapaJohnsCODE;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace PapaJohns
 {
@@ -31,24 +35,143 @@ namespace PapaJohns
            
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
 
+        //============================
+        //          CANVAS
+        //============================
+
+        // De-Serialize XML to UIElement using a given filename.
+        public static UIElement DeSerializeXAML(string filename)
+        {
+            // Load XAML from file. Use 'using' so objects are disposed of properly.
+            using (System.IO.FileStream fs = System.IO.File.Open(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                return System.Windows.Markup.XamlReader.Load(fs) as UIElement;
+            }
         }
 
-        private void SaveButton_2_Click(object sender, RoutedEventArgs e)
+        // Serializes any UIElement object to XAML using a given filename.
+        public static void SerializeToXAML(UIElement element, string filename)
         {
+            // Use XamlWriter object to serialize element
+            string strXAML = System.Windows.Markup.XamlWriter.Save(element);
 
+            // Write XAML to file. Use 'using' so objects are disposed of properly.
+            using (System.IO.FileStream fs = System.IO.File.Create(filename))
+            {
+                using (System.IO.StreamWriter streamwriter = new System.IO.StreamWriter(fs))
+                {
+                    streamwriter.Write(strXAML);
+                }
+            }
+        }
+
+
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "UIElement File"; // Default file name
+            dlg.DefaultExt = ".xaml"; // Default file extension
+            dlg.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                SerializeToXAML(designSpace, filename);
+            }
         }
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".xaml"; // Default file extension
+            dlg.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                Canvas canvas = DeSerializeXAML(filename) as Canvas;
+
+                // Add all child elements (lines, rectangles etc) to canvas
+                while (canvas.Children.Count > 0)
+                {
+                    UIElement obj = canvas.Children[0]; // Get next child
+                    canvas.Children.Remove(obj); // Have to disconnect it from result before we can add it
+                    designSpace.Children.Add(obj); // Add to canvas
+                }
+            }
+
         }
 
-        private void LoadButton_2_Click(object sender, RoutedEventArgs e)
+
+        //=========================
+        //       RELATIONS
+        //=========================
+        private void SerializeObject(string filename)
         {
 
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Mesa>));
+            List<Mesa> i = new List<Mesa>();
+
+            // Create an XmlTextWriter using a FileStream.
+            Stream fs = new FileStream(filename, FileMode.Create);
+            XmlWriter writer = new XmlTextWriter(fs, Encoding.Unicode);
+            // Serialize using the XmlTextWriter.
+            serializer.Serialize(writer, i);
+            writer.Close();
+        }
+
+        private void DeserializeObject(string filename)
+        {
+            // Create an instance of the XmlSerializer.
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Mesa>));
+
+
+            using (Stream reader = new FileStream(filename, FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                mesas = (List<Mesa>)serializer.Deserialize(reader);
+            }
+        }
+
+
+        private void SaveButton_2_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "UIElement File"; // Default file name
+            dlg.DefaultExt = ".xaml"; // Default file extension
+            dlg.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            dlg.ShowDialog();
+
+            SerializeObject(dlg.FileName);
+
+        }
+       
+     
+        private void LoadButton_2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.DefaultExt = ".xaml"; // Default file extension
+            ofd.Filter = "Xaml File (.xaml)|*.xaml"; // Filter files by extension
+
+            ofd.ShowDialog();
+
+            DeserializeObject(ofd.FileName);
         }
     }
 }
